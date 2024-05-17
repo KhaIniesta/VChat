ecf37a8087b857eec912b34c5f2c97ba0381b924
quythuong <quythuong0108@gmail.com>
2024-05-17 16:35:41 +0700

    showing search result

--- app/(app)/search.js
+++ app/(app)/search.js
@@ -3,48 +3,61 @@ import React, { useEffect, useRef, useState } from "react";
 import { ScrollView, TextInput } from "react-native-gesture-handler";
 import SearchList from "../../components/searchList";
 import firebase from "firebase/compat/app";
-import { collection } from "firebase/firestore";
+import { collection, doc, getDoc, getDocs, query, where } from "firebase/firestore";
 import { db } from "../../firebaseConfig";
+import { tintColorLight } from "../../constants/Colors";
 
 
 const Search = () => {
-  const [searchString, getSearchString] = useState("");
+  const [searchString, setSearchString] = useState("");
+  const [users, setUsers] = useState([]);
+  const [isTyping, setIsTyping] = useState(false);
 
-  const getUsernamesBySearchString = (searchString) => {
-    // const usersRef = collection(db, 'users');
-    // return collection(db, 'users')
-    // .where('username', 'array-contains', searchString)
-    // .get()
-    // .then(querySnapshot => {
-    //   const usernames = [];
-    //   querySnapshot.forEach(doc => usernames.push(doc.id));
-    //   return usernames;
-    // })
-    // .catch(error => {
-    //   console.error('Error getting usernames:', error);
-    //   return [];
-    // });
-  }
-  const loadSearchResult = () => {
-    console.log(searchString);
-    // get the username list by the typing
-    const usersArr = getUsernamesBySearchString(searchString);
+  const getUsernamesBySearchString = async() => {
+    console.log("search string: " + searchString);
+    setIsTyping(false);
+    if(searchString.length == 0) return;
+
+    const usersRef = await collection(db, "users");
+    const q = query(usersRef, where("username", ">=", searchString), where("username", "<=",searchString + "\uf8ff"));
+    const querySnapshot = await getDocs(q);
+
+    let matchingUsers = [];
+    querySnapshot.forEach((doc) => {
+      matchingUsers.push(doc.data());
+    });
     
+    setUsers(matchingUsers);
   }
 
   useEffect(() => {
-    const TimeOutId = setTimeout(loadSearchResult, 500);
+    setUsers([]);
+    setIsTyping(true);
+
+    const TimeOutId = setTimeout(getUsernamesBySearchString, 500);
     return () => clearTimeout(TimeOutId);
   },[searchString])
 
   return (
     <View className="flex-1 items-center p-3 mt-2">
         <TextInput 
-          onChangeText={value => setText(value)}
+          onChangeText={value => setSearchString(value)}
           style={{fontSize: 14}} 
           autoCapitalize="none" 
           placeholder="search by user name" 
           className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-2xl focus:ring-blue-500 focus:border-violet-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-red-500"/>
+          <View className="w-full flex-1 mt-3">
+            {
+              isTyping? (
+                  <View className="w-full flex-1 justify-center">
+                    <ActivityIndicator size='large' color={tintColorLight}></ActivityIndicator>
+                  </View>
+              ):
+              (
+                <SearchList users={users}/>
+              )
+            }
+          </View>
     </View>
   );
 };
--- components/searchItem.js
+++ components/searchItem.js
@@ -8,66 +8,23 @@ import { Image } from 'expo-image'
 import { blurhash, formatDate, getRoomId } from "../utils/common";
 import { collection, doc, onSnapshot, orderBy, query } from "firebase/firestore";
 import { db } from "../firebaseConfig";
+import { tintColorLight } from "../constants/Colors";
 
-const SearchItem = ({ item, router, noBorder}) => {
-//   const [lastMessage, setLastMessage] = useState(undefined)
-//   useEffect(() => {
-
-//     let roomId = getRoomId(currentUser?.userId, item?.userId);
-//     const docRef = doc(db, "rooms", roomId)
-//     const messagesRef = collection(docRef, "messages")
-//     const q = query(messagesRef, orderBy('createdAt', 'desc'))
-
-//     let unsub = onSnapshot(q, (snapshot) => {
-//         let allMessages = snapshot.docs.map(doc => {
-//             return doc.data()
-//         })
-//         setLastMessage(allMessages[0]? allMessages[0]: null)
-//     })
-//   }, []);
-
-//   const openChatRoom = async() => {
-//     router.push({pathname: '/chatRoom', params: item})
-//   }
-  
-//   const renderTime = () => {
-//     if (lastMessage) {
-//       let date = lastMessage?.createdAt
-//       return formatDate(new Date(date?.seconds*1000))
-//     }
-//   }
-
-//   const renderLastMessage  = () => {
-//     if (typeof lastMessage == 'undefined') return 'Loading ...'
-    
-//     if (lastMessage) {
-//       if (currentUser?.userId == lastMessage?.userId) return 'You: ' + lastMessage?.text 
-//       return lastMessage?.text
-//     }
-//     else {
-//       return 'Say hello 👋'
-//     }
-//   }
-
+const SearchItem = ({ user, router, noBorder}) => {
   return (
-    <Pressable onPress={openProfile}
-      className={`flex-row justify-between mx-4 items-center gap-3  mb-4 pb-2 ${
-        noBorder ? "" : "border-b border-neutral-300"
-      }`}
-    >
+    <Pressable onPress={() => console.log("open profile")} className="flex-row items-center gap-3 mb-4 pb-2 p-2 rounded-lg"
+    style={{backgroundColor: tintColorLight}}>
       <Image 
         style={{ height: hp(6), width: hp(6), borderRadius: 100}}
-        source={item?.profileUrl}
+        source={user?.profileUrl}
         placeholder={blurhash}
         transition={500}
       />
 
       <View className="flex-1 gap-1">
         <View className="flex-row justify-between">
-          <Text className="font-semibold text-neutral-600">{item?.username}</Text>
-          <Text className="font-medium text-neutral-400">{renderTime()}</Text>
+          <Text className="font-semibold text-neutral-100 text-xl pl-2">{user?.username}</Text>
         </View>
-        <Text className="font-medium text-neutral-400">{renderLastMessage()}</Text>
       </View>
     </Pressable>
   );
